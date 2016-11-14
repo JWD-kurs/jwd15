f42f59b012d2db29911ab2a721748a477fd97dd6
milansegedinac <milan.segedinac@gmail.com>
2016-11-14 22:25:05 +0100

    Lab 06 - sa casa

---
+++ angularSpecijalniUslovi/main.js
@@ -0,0 +1,22 @@
+var app = angular.module('wafepaApp', ['ngRoute']);
+app.config(function($routeProvider) {
+	// $routeProvider
+	// .when('/home',
+	// 	{templateUrl:'home.html'});
+	// console.log('pokrenula se angular aplikacija');
+});
+app.controller('myCtrl', function($scope){
+	$scope.ime='Pera';
+	$scope.prezime='Peric';
+	$scope.lista = ['asdaf','fds','asdf','asdffsd'];
+	$scope.obrisiIme = function(){
+		$scope.ime='';
+	};
+	$scope.proba = function(){
+		console.log('!!!');
+	};	
+});
+app.controller('myNewCtrl',function($scope){
+	$scope.poruka='hello world';
+});
+
